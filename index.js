6b33e41fd407549760cae31b2388e9134e32b29a
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-08-04 11:02:25 +0300

    working with function parameter in javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+// takes in an argument of a name and returns a phrase with that name using string interpolation
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+
+// takes in two arguments, a name and a language, and returns a phrase using those arguments
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+// takes in two arguments, a name and a language, and language defaults to JavaScript
+function introductionWithLanguageOptional(name, language){
+    if(language === undefined){
+        return `Hi, my name is ${name} and I am learning to program in JavaScript.`;
+    }
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
